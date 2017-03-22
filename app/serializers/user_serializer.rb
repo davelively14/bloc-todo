1797cb4c5c38090e05b3e6a18e6992467ca2eee5
Dave Lively <davelively14@gmail.com>
2017-03-22 13:23:06 -0400

    Added num_lists

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email
+  attributes :id, :username, :email, :num_lists
+
+  def num_lists
+    object.lists.count
+  end
 end
