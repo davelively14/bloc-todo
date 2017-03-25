ee635b07d39f945f06939531ec63a7961c8a339f
Dave Lively <davelively14@gmail.com>
2017-03-25 07:59:45 -0400

    Added modification authorization for List

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,19 +8,23 @@ class ApiController < ApplicationController
     authenticate_or_request_with_http_basic {|username, password| authorized?(username, password, request) }
   end
 
+  # Given a current_user and an ActionController::Request object, we determine
+  # if the current_user is modifiying an item, list, or user record. If so, we
+  # then determine if
   def modify?(current_user, request)
     if request.delete? || request.post? || request.put?
       case request.params["controller"]
       when "api/items"
-        # puts "params list_id: #{request.params["list_id"]}, method: #{request.method}, list's user_id: #{List.find(request.params["list_id"]).user_id}"
         user = request.post? ? List.find(request.params["list_id"]).get_user : Item.find(request.params["id"]).get_user
+        return current_user == user
+      when "api/lists"
+        user = request.post? ? User.find(request.params["user_id"]) : User.find(request.params["user_id"])
         return current_user == user
       when "api/users"
         puts "users"
-      when "api/lists"
-        puts "lists"
       else
-        puts "nothing above"
+        puts "This api is not "
+        false
       end
     else
       true
@@ -28,6 +32,8 @@ class ApiController < ApplicationController
     true
   end
 
+  # Authenticates user by passing the password to the :authenticate black box,
+  # and checks authorization via modify? method
   def authorized?(username, password, request)
     current_user = User.where(username: username).first
     current_user.try(:authenticate, password).present? && modify?(current_user, request)
--- spec/controllers/app/lists_controller_spec.rb
+++ spec/controllers/app/lists_controller_spec.rb
@@ -3,43 +3,55 @@ require 'rails_helper'
 RSpec.describe Api::ListsController, type: :controller do
   context "Authorized user" do
     before do
-      create(:user, username: "auth_user", password: "password", password_confirmation: "password")
+      @user = create(:user, username: "auth_user", password: "password", password_confirmation: "password")
       request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials("auth_user", "password")
     end
 
     describe "POST create" do
       it "adds new list" do
-        user = create(:user)
         name = Faker::Book.title
-        post :create, {user_id: user.id, list: {name: name}}
+        post :create, {user_id: @user.id, list: {name: name}}
         expect(response.body).to eq(ListSerializer.new(List.last).to_json)
+      end
+
+      it "cannot add a new list for another user" do
+        new_user = create(:user)
+        name = Faker::Book.title
+        post :create, {user_id: new_user.id, list: {name: name}}
+        expect(response.body).to eq("HTTP Basic: Access denied.\n")
       end
     end
 
     describe "DELETE destroy" do
       it "deletes a list" do
-        list = create(:list)
+        list = create(:list, user_id: @user.id)
         delete :destroy, id: list.id, user_id: list.user_id
         expect(List.where(id: list.id)).to eq([])
       end
 
       it "deletes associated items" do
-        list = create(:list)
+        list = create(:list, user_id: @user.id)
         2.times { create(:item, list: list) }
         expect{
           delete :destroy, id: list.id, user_id: list.user_id
         }.to change(Item, :count).by(-2)
       end
+
+      it "cannot delete another user's list" do
+        new_user = create(:user)
+        list = create(:list, user_id: new_user.id)
+        delete :destroy, id: list.id, user_id: list.user_id
+        expect(response.body).to eq("HTTP Basic: Access denied.\n")
+      end
     end
 
     describe "PUT update" do
       it "updates existing list" do
-        user = create(:user)
-        list = create(:list, user: user, name: "Old name", permissions: :priv)
+        list = create(:list, user: @user, name: "Old name", permissions: :priv)
         new_name = "New name"
         new_permissions = :pub
 
-        put :update, id: list.id, user_id: user.id, list: {name: new_name, permissions: new_permissions}
+        put :update, id: list.id, user_id: @user.id, list: {name: new_name, permissions: new_permissions}
         new_list = List.find(list.id)
 
         expect(new_list.name).to eq(new_name)
@@ -47,14 +59,23 @@ RSpec.describe Api::ListsController, type: :controller do
       end
 
       it "returns error with wrong invalid paramter" do
-        user = create(:user)
-        list = create(:list, user: user, name: "Old name", permissions: :priv)
+        list = create(:list, user: @user, name: "Old name", permissions: :priv)
         new_name = "New name"
         new_permissions = :not_supported
 
         expect{
-          put :update, id: list.id, user_id: user.id, list: {name: new_name, permissions: new_permissions}
+          put :update, id: list.id, user_id: @user.id, list: {name: new_name, permissions: new_permissions}
         }.to raise_error(ArgumentError)
+      end
+
+      it "cannot update a list belonging to another user" do
+        new_user = create(:user)
+        list = create(:list, user: new_user, name: "Old name", permissions: :priv)
+        new_name = "New name"
+        new_permissions = :pub
+
+        put :update, id: list.id, user_id: new_user.id, list: {name: new_name, permissions: new_permissions}
+        expect(response.body).to eq("HTTP Basic: Access denied.\n")
       end
     end
   end
